eb853580eeca2fe4833a8f6ea551fe8441c7dae7
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-11-10 19:26:08 -0300

    teste

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# ===== Inicialização =====
+'''# ===== Inicialização =====
 # ----- Importa e inicia pacotes
 import pygame
 import random
@@ -11,7 +11,7 @@ pygame.mixer.init()
 
 # ----- Gera tela principal
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('text based adventure game')
+pygame.display.set_caption('Jetpack Joyride')
 
 state = INIT
 while state != QUIT:
@@ -21,6 +21,145 @@ while state != QUIT:
         state = game_screen(window)
     else:
         state = QUIT
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados'''
+
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+
+pygame.init()
+
+# ----- Gera tela principal
+WIDTH = 480
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Jetpack Joyride')
+
+# ----- Inicia assets
+PLAYER_WIDTH = 50
+PLAYER_HEIGHT = 38
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('assets/img/background.jpg').convert()
+player_img = pygame.image.load('assets/img/player.png').convert_alpha()
+player_img = pygame.transform.scale(player_img, (PLAYER_WIDTH, PLAYER_HEIGHT))
+raio1 = pygame.image.load('assets/img/zap1.png').convert_alpha()
+raio2 = pygame.image.load('assets/img/zap2.png').convert_alpha()
+raio3 = pygame.image.load('assets/img/zap3.png').convert_alpha()
+raio4 = pygame.image.load('assets/img/zap4.png').convert_alpha()
+
+# ----- Inicia estruturas de dados
+# Definindo os novos tipos
+class Player(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centery = HEIGHT / 2
+        self.rect.left = WIDTH - 400
+        self.speedy = 0
+
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.y += self.speedy
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+class Raio(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(WIDTH+100, PLAYER_WIDTH)
+        self.rect.y = random.randint(0, HEIGHT-PLAYER_HEIGHT)
+        self.speedx = random.randint(-3, -23)
+        #self.speedy = random.randint(2, 9)
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        #self.rect.y += self.speedy
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randint(WIDTH+100, PLAYER_WIDTH)
+            self.rect.y = random.randint(0, HEIGHT-PLAYER_HEIGHT)
+            self.speedx = random.randint(-5, -20)
+            #self.speedy = random.randint(2, 9)
+
+game = True
+# Variável para o ajuste de velocidade
+clock = pygame.time.Clock()
+FPS = 30
+
+# Criando um grupo de meteoros
+all_sprites = pygame.sprite.Group()
+# Criando o jogador
+player = Player(player_img)
+all_sprites.add(player)
+# Criando os meteoros
+raios = [raio1,raio2,raio3,raio4]
+for i in range(8):
+    raio = Raio(random.choice(raios))
+    all_sprites.add(raio)
+
+# ===== Loop principal =====
+while game:
+    clock.tick(FPS)
+
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx += 8
+            if event.key == pygame.K_UP:
+                player.speedy -= 8
+            if event.key == pygame.K_DOWN:
+                player.speedy += 8
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx -= 8
+            if event.key == pygame.K_UP:
+                player.speedy += 8
+            if event.key == pygame.K_DOWN:
+                player.speedy -= 8
+
+    # ----- Atualiza estado do jogo
+    # Atualizando a posição dos meteoros
+    all_sprites.update()
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    # Desenhando meteoros
+    all_sprites.draw(window)
+
+    pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
