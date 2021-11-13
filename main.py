6bead047902c9b684e46421fa6077b37dd062a73
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-11-13 15:16:35 -0300

    Tiro e Explosão

--- main.py
+++ main.py
@@ -49,27 +49,36 @@ player_img = pygame.image.load('assets/img/walk1.png').convert_alpha()
 img_fly = pygame.image.load('assets/img/flying1.png').convert_alpha()
 img_jump = pygame.image.load('assets/img/beggining.png').convert_alpha()
 # ---- Obstacles
-raio1 = pygame.image.load('assets/img/zap1.png').convert_alpha()
-raio2 = pygame.image.load('assets/img/zap2.png').convert_alpha()
-raio3 = pygame.image.load('assets/img/zap3.png').convert_alpha()
-raio4 = pygame.image.load('assets/img/zap4.png').convert_alpha()
-raio3 = pygame.image.load('assets/img/zap5.png').convert_alpha()'''
+Rocket1 = pygame.image.load('assets/img/zap1.png').convert_alpha()
+Rocket2 = pygame.image.load('assets/img/zap2.png').convert_alpha()
+Rocket3 = pygame.image.load('assets/img/zap3.png').convert_alpha()
+Rocket4 = pygame.image.load('assets/img/zap4.png').convert_alpha()
+Rocket3 = pygame.image.load('assets/img/zap5.png').convert_alpha()'''
 
 assets = {}
 assets['background'] = pygame.image.load('assets/img/background.jpg').convert()
 assets['ground'] = pygame.image.load('assets/img/background_ground.png').convert_alpha()
-assets['background_move'] = pygame.image.load('assets/img/background_move.png').convert_alpha()
+#assets['background_move'] = pygame.image.load('assets/img/background_move.png').convert_alpha()
 assets['player_img'] = pygame.image.load('assets/img/walk1.png').convert_alpha()
 assets['img_fly'] = pygame.image.load('assets/img/flying1.png').convert_alpha()
 assets['img_jump'] = pygame.image.load('assets/img/beggining.png').convert_alpha()
 assets['rocket_img'] = pygame.image.load('assets/img/foguete.png').convert_alpha()
 assets['rocket_img'] = pygame.transform.scale(assets['rocket_img'], (47*3, 24*3))
+assets['bullet_img'] = pygame.image.load('assets/img/star.png').convert_alpha()
+assets['bullet_img'] = pygame.transform.scale(assets['bullet_img'], (47, 47))
+explosion_anim = []
+for i in range(9):
+    img = pygame.image.load(f'assets/img/exp{i}.png').convert()
+    img = pygame.transform.scale(img, (90, 90))
+    explosion_anim.append(img)
+assets["explosion_anim"] = explosion_anim
+
 
 # Carrega os sons do jogo
 pygame.mixer.music.load('assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
 pygame.mixer.music.set_volume(0.4)
-boom_sound = pygame.mixer.Sound('assets/snd/expl3.wav')
-#destroy_sound = pygame.mixer.Sound('assets/snd/expl6.wav')
+assets['boom_sound'] = pygame.mixer.Sound('assets/snd/expl3.wav')
+assets['destroy_sound'] = pygame.mixer.Sound('assets/snd/expl6.wav')
 pew_sound = pygame.mixer.Sound('assets/snd/pew.wav')
 assets['pew_sound'] = pygame.mixer.Sound('assets/snd/pew.wav')
 
@@ -77,8 +86,8 @@ assets['pew_sound'] = pygame.mixer.Sound('assets/snd/pew.wav')
 ground_scroll = 0
 scroll_speed = 5
 dead = False
-#raio_frequency = 4000 # --> Milesegundos 
-#last_raio = pygame.time.get_ticks()
+#Rocket_frequency = 4000 # --> Milesegundos 
+#last_Rocket = pygame.time.get_ticks()
 
 # ---- Inicia estruturas de dados
 # ---- Definindo as classes
@@ -143,8 +152,15 @@ class Player(pygame.sprite.Sprite):
     
     def fogo(self):
         self.assets['pew_sound'].play()
+    
+    def shoot(self):
+        # A nova bala vai ser criada logo acima e no centro horizontal da nave
+        new_bullet = Bullet(self.assets, self.rect.right, self.rect.centery)
+        self.groups['all_sprites'].add(new_bullet)
+        self.groups['all_bullets'].add(new_bullet)
+        self.assets['pew_sound'].play()
 
-class Raio(pygame.sprite.Sprite):
+class Rocket(pygame.sprite.Sprite):
     def __init__(self,assets):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
@@ -168,6 +184,74 @@ class Raio(pygame.sprite.Sprite):
             self.rect.x = random.randint(1024, 1536)
             self.rect.y = random.randint(110, 630)
             
+# Classe Bullet que representa os tiros
+class Bullet(pygame.sprite.Sprite):
+    # Construtor da classe.
+    def __init__(self, assets, left, centery):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets['bullet_img']
+        self.rect = self.image.get_rect()
+
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.centery = centery
+        self.rect.left = left
+        self.speedx = 10  # Velocidade fixa para a direita
+
+    def update(self):
+        # A bala só se move no eixo x
+        self.rect.x += self.speedx
+
+        # Se o tiro passar do fim da tela, morre.
+        if self.rect.left > WIDTH:
+            self.kill()
+
+# Classe que representa uma explosão de meteoro
+class Explosion(pygame.sprite.Sprite):
+    # Construtor da classe.
+    def __init__(self, center, assets):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        # Armazena a animação de explosão
+        self.explosion_anim = assets['explosion_anim']
+
+        # Inicia o processo de animação colocando a primeira imagem na tela.
+        self.frame = 0  # Armazena o índice atual na animação
+        self.image = self.explosion_anim[self.frame]  # Pega a primeira imagem
+        self.rect = self.image.get_rect()
+        self.rect.center = center  # Posiciona o centro da imagem
+
+        # Guarda o tick da primeira imagem, ou seja, o momento em que a imagem foi mostrada
+        self.last_update = pygame.time.get_ticks()
+
+        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
+        self.frame_ticks = 50
+
+    def update(self):
+        # Verifica o tick atual.
+        now = pygame.time.get_ticks()
+        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
+        elapsed_ticks = now - self.last_update
+
+        # Se já está na hora de mudar de imagem...
+        if elapsed_ticks > self.frame_ticks:
+            # Marca o tick da nova imagem.
+            self.last_update = now
+
+            # Avança um quadro.
+            self.frame += 1
+
+            # Verifica se já chegou no final da animação.
+            if self.frame == len(self.explosion_anim):
+                self.kill()
+            else:
+                # Se ainda não chegou ao fim da explosão, troca de imagem.
+                center = self.rect.center
+                self.image = self.explosion_anim[self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
 
 # Variável para o ajuste de velocidade (FPS)
 clock = pygame.time.Clock()
@@ -176,22 +260,29 @@ FPS = 60
 # ---- Criando um grupo de sprites
 all_sprites = pygame.sprite.Group()
 all_rockets = pygame.sprite.Group()
+all_bullets = pygame.sprite.Group()
 
 groups = {}
 groups['all_sprites'] = all_sprites
 groups['all_rockets'] = all_rockets
+groups['all_bullets'] = all_bullets
 
 # ---- Criando o jogador
 player = Player(groups,assets)
 all_sprites.add(player)
 
-# ---- Criando os raios
-#raios = [raio1, raio2]
+# ---- Criando os Rockets
+#Rockets = [Rocket1, Rocket2]
 for _ in range(3):
-    #raio = Raio(random.choice(raios))
-    rocket = Raio(assets)
+    #Rocket = Rocket(random.choice(Rockets))
+    rocket = Rocket(assets)
     all_sprites.add(rocket)
     all_rockets.add(rocket)
+
+'''DONE = 0
+PLAYING = 1
+EXPLODING = 2
+state = PLAYING'''
 
 # ===== Loop principal =====
 game = True
@@ -212,6 +303,8 @@ while game:
             if event.key == pygame.K_UP:
                 player.speedy -= 20
                 player.fogo()
+            if event.key == pygame.K_SPACE:
+                player.shoot()
                 
         # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
@@ -219,10 +312,7 @@ while game:
             if event.key == pygame.K_UP:
                 player.speedy += 20
 
-    # ---- Verifica se houve colisão entre nave e meteoro
-    
-
-    # Finalizar jogo se colidir player com raios - !!!IMPLEMENTAR!!!
+    # Finalizar jogo se colidir player com Rockets - !!!IMPLEMENTAR!!!
     #if len(hits) == True:
         #dead = True
 
@@ -231,10 +321,23 @@ while game:
     all_sprites.update()
 
 
+    # Verifica se houve colisão entre tiro e foguete
+    hits = pygame.sprite.groupcollide(all_rockets, all_bullets, True, True)
+    for rocket in hits: # As chaves são os elementos do primeiro grupo (rockets) que colidiram com alguma bala
+        # O meteoro e destruido e precisa ser recriado
+        assets['destroy_sound'].play()
+        r = Rocket(assets)
+        all_sprites.add(r)
+        all_rockets.add(r)
+        
+        explosao = Explosion(rocket.rect.center, assets)
+        all_sprites.add(explosao)
+    
+    # ---- Verifica se houve colisão entre player e foguete
     hits = pygame.sprite.spritecollide(player, all_rockets, True)
     if len(hits) > 0:
         dead = True
-        boom_sound.play()
+        assets['boom_sound'].play()
         time.sleep(1) # Precisa esperar senão fecha
         game = False
         
@@ -244,11 +347,11 @@ while game:
     window.blit(assets['ground'], (ground_scroll, 0))
     
     if dead == False:
-        # ---- Cria novos raios
+        # ---- Cria novos Rockets
         #time_now = pygame.time.get_ticks()
-        #if time_now - last_raio > raio_frequency:
-                #raio = Raio(random.choice(raios))
-                #all_sprites.add(raio)
+        #if time_now - last_Rocket > Rocket_frequency:
+                #Rocket = Rocket(random.choice(Rockets))
+                #all_sprites.add(Rocket)
         # ---- Faz o background se mover
         ground_scroll -= scroll_speed
         if abs(ground_scroll) > 1024:
