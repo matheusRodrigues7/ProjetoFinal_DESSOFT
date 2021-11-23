dad6a9721a671449e2af7f175c4304e385c384df
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-11-23 13:27:57 -0300

    Leaderboards

--- final_screen.py
+++ final_screen.py
@@ -3,54 +3,55 @@ import sys
 import random
 from os import path
 from assets import BACKGROUND, load_assets, SCORE_FONT_FINAL, SCORE_FONT_LEADERBOARDS, GROUND
-from config import IMG_DIR, BLACK, FPS, GAME, QUIT, WIDTH, HEIGHT, FNT_DIR, WHITE
+from config import IMG_DIR, BLACK, FPS, GAME, FNT_DIR, WHITE, YELLOW, GRAY
 from game_screen import game_screen
 
-scores = [0,0,0]
+# Lista de 3 maiores scores
+scores = []
 
 def final_screen(screen,score):
-    with open('leaderboards.txt', 'a') as arquivo:
-        arquivo.write(f"{score}\n")
-    
+    if len(scores) < 3:
+        scores.append(score)
+
     if score > scores[0]:
         scores.insert(1,scores[0])
         scores.remove(scores[-1])
         scores.remove(scores[0])
         scores.insert(0,score)
-    elif score > scores[1]:
+    elif len(scores) > 1 and score > scores[1]:
         scores.insert(2,scores[1])
         scores.remove(scores[-1])
         scores.remove(scores[1])
         scores.insert(1,score)
-    elif score > scores[2]:
+    elif len(scores) > 2 and score > scores[2]:
         scores.remove(scores[2])
         scores.insert(2,score)
 
     # Variável para o ajuste de velocidade
     clock = pygame.time.Clock()
+    
+    assets = load_assets()
 
     # Carrega o fundo da tela inicial
     background = pygame.image.load(path.join(IMG_DIR, 'morte.png')).convert_alpha()
     background_rect = background.get_rect()
 
-    running = True
     pygame.init() 
     res = (1024,768) 
     screen = pygame.display.set_mode(res)
-    color = (255,255,0) 
-    color_light = (170,170,170) 
-    color_dark = (100,100,100) 
     #width = screen.get_width() 
     #height = screen.get_height() 
     smallfont = pygame.font.Font(path.join(FNT_DIR, 'NewAthleticM54.ttf'), 80)
-    text = smallfont.render('QUIT' , True , color)
-    text2 = smallfont.render('PLAY AGAIN' , True , color)
+    sair = smallfont.render('QUIT', True, BLACK)
+    play_again = smallfont.render('PLAY AGAIN', True , BLACK)
+    
+    running = True
+    pygame.mixer.music.play(loops=-1)
     while running:
 
         # Ajusta a velocidade do jogo.
         clock.tick(FPS)
 
-        assets = load_assets()
         # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
@@ -74,33 +75,28 @@ def final_screen(screen,score):
 
         screen.blit(background, background_rect)
 
-        '''text_surface = assets[SCORE_FONT_FINAL].render(f'{str(score)}M ', True, (255, 255, 0))
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (250,  150)
-        screen.blit(text_surface, text_rect)'''
-
         if 524 <= mouse[0] <= 524+365 and 320 <= mouse[1] <= 320+155: 
-            pygame.draw.rect(screen,color_light,[524,320,365,155]) 
+            pygame.draw.rect(screen,WHITE,[524,320,365,155]) 
           
         else: 
-            pygame.draw.rect(screen,color_dark,[524,320,365,155])
+            pygame.draw.rect(screen,GRAY,[524,320,365,155])
         
         if 524 <= mouse[0] <= 524+365 and 495 <= mouse[1] <= 495+155: 
-            pygame.draw.rect(screen,color_light,[524,528,450,158]) 
+            pygame.draw.rect(screen,WHITE,[524,528,450,158]) 
           
         else: 
-            pygame.draw.rect(screen,color_dark,[524,528,450,158])
+            pygame.draw.rect(screen,GRAY,[524,528,450,158])
         
-        screen.blit(text , (640.5,350.5))
-        screen.blit(text2 , (570.5,560.5))
-        text_surface = assets[SCORE_FONT_FINAL].render(f'{str(score)}M ', True, (255, 255, 0))
+        screen.blit(sair, (640.5,350.5))
+        screen.blit(play_again , (570.5,560.5))
+        text_surface = assets[SCORE_FONT_FINAL].render(f'{str(int(score))}M ', True, YELLOW)
         text_rect = text_surface.get_rect()
         text_rect.midtop = (250,  150)
         screen.blit(text_surface, text_rect)
 
         n = 125
         for score1 in scores:
-            text_surface = assets[SCORE_FONT_LEADERBOARDS].render(f'{str(score1)}M ', True, WHITE)
+            text_surface = assets[SCORE_FONT_LEADERBOARDS].render(f'{str(int(score1))}M ', True, YELLOW)
             text_rect = text_surface.get_rect()
             text_rect.midtop = (800,  n)
             screen.blit(text_surface, text_rect)
@@ -111,8 +107,6 @@ def final_screen(screen,score):
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
         
-        
-
     return state
 
     # text_surface = load_assets()[SCORE_FONT].render("{:08d}".format(int(game_screen.score)), True, (255, 255, 0))
